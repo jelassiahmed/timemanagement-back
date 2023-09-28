92e9ce5d6e6c8dd4196a690c85aeed8f1e7955fb
jelassiahmed <ahmed.jelassi@esprit.tn>
2023-09-29 00:32:13 +0100

    added controller

---
+++ src/main/java/com/timemanagemenet/timemanagementapp/Controller/EnfantController.java
@@ -0,0 +1,37 @@
+package com.timemanagemenet.timemanagementapp.Controller;
+
+import com.timemanagemenet.timemanagementapp.Entity.Enfant;
+import com.timemanagemenet.timemanagementapp.Entity.EnfantId;
+import com.timemanagemenet.timemanagementapp.Service.Enfant.EnfantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enfants")
+public class EnfantController {
+
+    @Autowired
+    private EnfantService enfantService;
+
+    @GetMapping
+    public List<Enfant> getAllEnfants() {
+        return enfantService.getAllEnfants();
+    }
+
+    @GetMapping("/{id}")
+    public Enfant getEnfantById(@PathVariable EnfantId id) {
+        return enfantService.getEnfantById(id);
+    }
+
+    @PostMapping
+    public void saveEnfant(@RequestBody Enfant enfant) {
+        enfantService.saveEnfant(enfant);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEnfant(@PathVariable EnfantId id) {
+        enfantService.deleteEnfant(id);
+    }
+}
