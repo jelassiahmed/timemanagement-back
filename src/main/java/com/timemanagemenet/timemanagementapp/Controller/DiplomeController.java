7a3011e9b1cdfd33da241c3fc19b3b865141e0a8
jelassiahmed <ahmed.jelassi@esprit.tn>
2023-09-29 00:13:56 +0100

    added controller

---
+++ src/main/java/com/timemanagemenet/timemanagementapp/Controller/DiplomeController.java
@@ -0,0 +1,34 @@
+package com.timemanagemenet.timemanagementapp.Controller;
+
+import com.timemanagemenet.timemanagementapp.Entity.Diplome;
+import com.timemanagemenet.timemanagementapp.Service.Diplome.DiplomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("diplomes")
+public class DiplomeController {
+
+    @Autowired
+    private DiplomeService diplomeService;
+
+    @GetMapping("/{id}")
+    public Diplome getDiplomeById(@PathVariable Long id) {
+        return diplomeService.getDiplomeById(id);
+    }
+
+    @PostMapping
+    public void saveDiplome(@RequestBody Diplome diplome) {
+        diplomeService.saveDiplome(diplome);
+    }
+
+    @PutMapping("/{id}")
+    public void updateDiplome(@PathVariable Long id, @RequestBody Diplome updatedDiplome) {
+        diplomeService.updateDiplome(id, updatedDiplome);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDiplome(@PathVariable Long id) {
+        diplomeService.deleteDiplome(id);
+    }
+}
