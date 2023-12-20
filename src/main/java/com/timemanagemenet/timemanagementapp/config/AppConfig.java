93577b4210bff08de0c91eea178afc4b8fd7b3cf
jelassiahmed <ahmed.jelassi@esprit.tn>
2023-12-20 12:10:20 +0100

    minor fixes

--- src/main/java/com/timemanagemenet/timemanagementapp/config/AppConfig.java
+++ src/main/java/com/timemanagemenet/timemanagementapp/config/AppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
+    public KeycloakConfigResolver keycloakconfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
 
